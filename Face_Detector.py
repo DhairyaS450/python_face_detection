ac73a081e3000fa6c903b066610e2fbbf218a2b8
DhairyaS450 <dhairyashah2513@gmail.com>
2024-10-11 17:33:34 -0400

    added smile detection

--- Face_Detector.py
+++ Face_Detector.py
@@ -3,6 +3,7 @@ from random import randrange
 
 # Load some pre-trained data on face frontals from opencv (haar cascade algorithm)
 trained_face_data = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+trained_smile_data = cv2.CascadeClassifier('haarcascade_smile.xml')
 
 # Choose an image to detect faces in
 #img = cv2.imread('RDJ.png')
@@ -21,10 +22,21 @@ while True:
     # Detect faces
     face_coordinates = trained_face_data.detectMultiScale(grayscaled_img)
 
+    # Detect smiles
+    smile_coordinates = trained_smile_data.detectMultiScale(grayscaled_img, 1.5, 10)
+
     # Draw rectangles around the faces
     for (x, y, w, h) in face_coordinates:
         cv2.rectangle(frame, (x, y), (x+w, y+h), (randrange(256), randrange(256), randrange(256)))
     
+
+        # Draw rectangles around the smiles
+        for (x2, y2, w2, h2) in smile_coordinates:
+            # Only show if smile is detected within the coordinates of the face
+            if (x2+w2 >= x and x2+w2 <= x+w and y2+h2 >= y and y2+h2 <= y+h):
+                cv2.rectangle(frame, (x2, y2), (x2+w2, y2+h2), (randrange(256), randrange(256), randrange(256)))
+    
+
     cv2.imshow('Face Detector', frame)
     key = cv2.waitKey(1)
 
